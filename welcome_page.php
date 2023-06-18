7b749427ff7eac2f0ad8cb1b554e564abf912dda
kanita04 <131246493+kanita04@users.noreply.github.com>
2023-06-18 19:43:49 +0300

    Add files via upload

---
+++ welcome_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title></title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Welcome to Duo Pharm</h1>
+        <h2>Login as:</h2>
+        <a href="patient_login.php"><button type="button">Patient</button></a><br><br><br>
+        <a href="https://www.google.com"><button type="button">Doctor</button></a><br><br><br>
+        <a href="https://www.google.com"><button type="button">Pharmacist</button></a><br><br><br>
+    </body>
+</html>
